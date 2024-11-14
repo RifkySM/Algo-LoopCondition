b2159cbecdd73a70b2218c5c8235232a6fb64fdd
RifkySaputraMaylandra <andra04mei05@gmail.com>
2024-11-15 00:31:23 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@
  *
  * */
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -20,7 +20,7 @@ public class Main {
         int total_bayar = 0, num = 1;
         Scanner scan = new Scanner(System.in);
         boolean lanjut = true;
-        HashMap<String, String> daftar_belanja = new HashMap<>();
+        LinkedHashMap<String, String> daftar_belanja = new LinkedHashMap<>();
 
         do {
             System.out.println("Daftar Barang : \n 1. Pensil \t Rp.1000 \n 2. Buku \t Rp.3000 \n 3. Spidol \t Rp.5000");
@@ -36,19 +36,19 @@ public class Main {
                 continue;
             }
 
-            int harga = pilihan == 1 ? 1000: pilihan == 2 ? 3000 : pilihan == 3 ? 5000 :0 ;
-            String barang = pilihan == 1 ? "Pensil": pilihan == 2 ? "Buku" : pilihan == 3 ? "Spidol" : "Tidak Diketahui" ;
-            daftar_belanja.put(num+"."+barang, "Rp." + harga);
+            int harga = pilihan == 1 ? 1000 : pilihan == 2 ? 3000 : pilihan == 3 ? 5000 : 0;
+            String barang = pilihan == 1 ? "Pensil" : pilihan == 2 ? "Buku" : pilihan == 3 ? "Spidol" : "Tidak Diketahui";
+            daftar_belanja.put(num + ". " + barang, "Rp. " + harga);
             num++;
             total_bayar += harga;
 
-            System.out.println("Total Bayar : "+total_bayar);
+            System.out.println("Total Bayar : " + total_bayar);
             System.out.println("Lanjut ? (Y/n)");
-            lanjut = scan.next().equals("Y");
-        }while (lanjut);
+            lanjut = scan.next().equalsIgnoreCase("Y");
+        } while (lanjut);
 
         scan.close();
-        System.out.println("Daftar Barang yang dibeli");
+        System.out.println("Daftar Barang yang dibeli:");
         daftar_belanja.forEach((barang, harga) -> System.out.println(barang + " \t \t" + harga));
         System.out.println("-------------------------------------------");
         System.out.println("Total Yang Harus Dibayar : " + total_bayar);
